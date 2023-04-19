382545af343fce049cd786b124be91b1c3aab284
priscillaanpt <priscillanapitupuluu@gmail.com>
2023-04-19 11:22:24 +0700

    Addroom

--- src/entity/Room.java
+++ src/entity/Room.java
@@ -1,5 +1,49 @@
 package entity;
 
+import java.util.*;
+
 public class Room {
+    private String name;
+    private int width;
+    private Furniture[][] grid;
+    private Point locationRoom;
+    private List<Furniture> objects;
     
+    public Room(String name, int width, Point locationRoom) {
+        this.name = name;
+        this.width = width;
+        this.grid = new Furniture[width][width];
+        this.locationRoom = locationRoom;
+        this.objects = new ArrayList<>();
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public Point getLocation() {
+        return this.locationRoom;
+    }
+    
+    public void addObject(Furniture item, Point location) {
+        if (location.x >= 0 && location.x < width && location.y >= 0 && location.y < width) {
+            if (grid[location.x][location.y] == null) {
+                grid[location.x][location.y] = item;
+                item.setLocation(location);
+                objects.add(item);
+            } else {
+                System.out.println("Cannot add furniture, location already occupied.");
+            }
+        } else {
+            System.out.println("Cannot add furniture, location is out of bounds.");
+        }
+    }
+    
+    public void editObject(Furniture item) {
+        if (objects.contains(item)) {
+            // Edit furniture item
+        } else {
+            System.out.println("Furniture item not found in house.");
+        }
+    }
 }
