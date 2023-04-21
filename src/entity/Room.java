37a203a66ad42356d30f7c31ec341638bf68ef88
haidar2003 <muhammadrafihaidar03@gmail.com>
2023-04-21 15:00:31 +0700

    Room

--- src/entity/Room.java
+++ src/entity/Room.java
@@ -3,6 +3,7 @@ package entity;
 import java.util.*;
 
 import entity.item.furniture.Furniture;
+import entity.sim.Sim;
 
 public class Room {
     private int roomWidth = 6;
@@ -10,6 +11,7 @@ public class Room {
     private Furniture[][] grid;
     private Point locationRoom;
     private ArrayList<Furniture> objects;
+    Scanner scanner = new Scanner(System.in);
     
     public Room(String name, Point locationRoom) {
         this.name = name;
@@ -26,7 +28,15 @@ public class Room {
         return this.locationRoom;
     }
 
-    public void addObject(Furniture object, Point location) throws RoomException {
+    public void addObject(Furniture object) throws RoomException {
+        int x,y;
+        System.out.printf("\nMasukkan x: ");
+        x = scanner.nextInt();
+        System.out.printf("\nMasukkan y: ");
+        y = scanner.nextInt();
+
+        Point location = new Point(x, y);
+
         int objectWidth, objectLength;
         if (object.getRotation()) {
             // object is rotated
@@ -50,7 +60,7 @@ public class Room {
             }
         }
 
-        // No collision, add the object to the grid
+        // No collision
         for (int i = location.getX(); i < location.getX() + objectWidth; i++) {
             for (int j = location.getY(); j < location.getY() + objectLength; j++) {
                 grid[i][j] = object;
@@ -61,12 +71,68 @@ public class Room {
 
         objects.add(object);
     }
+
+    public void removeObject(Furniture object) {
+        Point location = object.getPoint();
+        int objectWidth, objectLength;
     
-    public void editObject(Furniture item) {
-        if (objects.contains(item)) {
-            // Edit furniture item
+        if (object.getRotation()) {
+            // object is rotated
+            objectWidth = object.getLength();
+            objectLength = object.getWidth();
         } else {
-            System.out.println("Furniture item not found in house.");
+            // object is not rotated
+            objectWidth = object.getWidth();
+            objectLength = object.getLength();
+        }
+    
+        for (int i = location.getX(); i < location.getX() + objectWidth; i++) {
+            for (int j = location.getY(); j < location.getY() + objectLength; j++) {
+                grid[i][j] = null;
+            }
+        }
+    
+        objects.remove(object);
+    }
+
+    public void editRoom(Sim sim){
+        System.out.printf("\nPilih aksi:\n1. Letakkan Objek\n2. Pindahkan Objek\n3. Ambil Objek\nNomor aksi:");
+        int x = scanner.nextInt();
+        while ((x < 1)||(x > 3)){
+            System.out.printf("\n\nSilakan ulangi input: ");
+            x = scanner.nextInt();
+        }
+
+        if (x == 1){
+            Furniture furniture = sim.getInventory().chooseFurniture();
+            if (furniture != null){
+                printRoom();
+                try {
+                    this.addObject(furniture);
+                } catch (RoomException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void printRoom() {
+        System.out.print("\nTampilan ruangan:\n");
+        for (int i = 0; i < roomWidth; i++) {
+            System.out.print(" " + i);
+        }
+        System.out.println();
+        for (int i = 0; i < roomWidth; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < roomWidth; j++) {
+                if (grid[i][j] == null) {
+                    System.out.print("[ ]");
+                } else {
+                    System.out.print("[-]");
+                }
+            }
+            System.out.printf("\n\n");
         }
     }
 }
--- src/entity/sim/InventoryManager.java
+++ src/entity/sim/InventoryManager.java
@@ -1,11 +1,15 @@
 package entity.sim;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import entity.item.Item;
+import entity.item.food.Food;
+import entity.item.furniture.Furniture;
 
 public class InventoryManager {
     ArrayList<Item> inventory = new ArrayList<>();
+    Scanner scanner = new Scanner(System.in);
 
     public ArrayList<Item> getList() {
         return inventory;
@@ -46,6 +50,41 @@ public class InventoryManager {
             System.out.println("Tidak ada item " + removedItem.getName() +" di inventory");
         }
     }
+
+    public Furniture chooseFurniture(){
+        System.out.println("Furnitur yang ada di inventory");
+        int count = 1;
+        for (Item item : inventory){
+            if (item instanceof Furniture){
+                Furniture furniture = (Furniture) item;
+                System.out.printf("%d. %s\n", count, furniture.getName());
+                count++;
+            }
+        }
+
+        if (count == 1){
+            System.out.println("Tidak ada furnitur");
+            return null;
+        } else{
+            System.out.printf("\nMasukkan nama furnitur: ");
+            String furnitureName = scanner.nextLine();
+
+            while (true){
+                System.out.printf("\nTidak ada furnitur dengan nama %s\nMasukkan nama furnitur: ", furnitureName);
+                furnitureName = scanner.nextLine();
+                for (Item item : inventory){
+                    if (furnitureName.equals(item.getName())){
+                        removeItem(item, 1);
+                        return (Furniture) item;
+                    }
+                }
+            }
+        }
+    }
+
+    // public Food chooseFood(){
+    //     System.out.println("Makanan yang ada di inventory");
+    // }
 
     public void removeList(ArrayList<Item> arrayList){
         for (Item item : arrayList){
