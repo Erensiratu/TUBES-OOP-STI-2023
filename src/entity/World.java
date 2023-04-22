4159f80738934470b0a09f3caa006940383e7210
haidar2003 <muhammadrafihaidar03@gmail.com>
2023-04-22 08:26:01 +0700

    World WIP

--- src/entity/World.java
+++ src/entity/World.java
@@ -1,5 +1,24 @@
 package entity;
 
+import java.util.ArrayList;
+
+import entity.sim.Sim;
+
 public class World {
+    final int WORLD_LENGTH = 64;
+    final int WORLD_WIDTH = 64;
+    ArrayList<House> listHouse;
+    ArrayList<Sim> listSim;
+    private int time;
+    private int dayPassed;
+
+    private static World instance = new World();
+
+    private World(){}
+
+    public static World getInstance(){
+        return instance;
+    }
+
     
 }
