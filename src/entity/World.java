c83d4c0640615babb62bed276c8473b733dde965
Iyaq1 <93713795+Iyaq1@users.noreply.github.com>
2023-04-24 23:25:01 +0700

    Add files via upload

--- src/entity/World.java
+++ src/entity/World.java
@@ -2,23 +2,56 @@ package entity;
 
 import java.util.ArrayList;
 
+import org.w3c.dom.ls.LSInput;
+
 import entity.sim.Sim;
 
 public class World {
     final int WORLD_LENGTH = 64;
     final int WORLD_WIDTH = 64;
-    ArrayList<House> listHouse;
-    ArrayList<Sim> listSim;
-    private int time;
-    private int dayPassed;
+    private ArrayList<House> listHouse ;
+    private ArrayList<Sim> listSim;
+    private Timer clock;
 
-    private static World instance = new World();
+    private static World instance ;
 
-    private World(){}
-
+    private World(){
+        Timer.init();
+        clock = Timer.getInstance();
+        listHouse = new ArrayList<House>();
+        listSim = new ArrayList<Sim>();
+    }
+    private World(Timer clock, ArrayList<House> listHouse, private ArrayList<Sim> listSim){
+        this.clock = clock;
+        this.listHouse = listHouse;
+        this.listSim = listSim;
+    }
     public static World getInstance(){
         return instance;
     }
-
-
+    public synchronized static void init(){
+        if (instance == null){
+            instance = new World();
+        }
+    }
+    public synchronized static void init(Timer clock, ArrayList<House> listHouse, private ArrayList<Sim> listSim){
+        if (instance == null){
+            instance = new World(clock,listHouse,listSim);
+        }
+    }
+    public synchronized void addHouse(House h){
+        listHouse.add(h);
+    }
+    public synchronized void addSim(Sim s){
+        listSim.add(s);
+    }
+    public ArrayList<House> getListHouse(){
+        return listHouse;
+    }
+    public ArrayList<Sim> getListSim(){
+        return listSim;
+    }
+    public int getTime(){
+        return clock.getTime();
+    }
 }
