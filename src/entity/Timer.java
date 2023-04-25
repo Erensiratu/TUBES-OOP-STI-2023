c84e32f3b1b30097139079cc2ede3df9616b4df3
Iyaq1 <93713795+Iyaq1@users.noreply.github.com>
2023-04-25 11:14:16 +0700

    Add files via upload

--- src/entity/Timer.java
+++ src/entity/Timer.java
@@ -3,60 +3,58 @@ package entity;
 import java.util.ArrayList;
 import java.util.List;
 
+import entity.sim.Sim;
+
 public class Timer {
-    private int count;
-    private List<Worker> workerList;
+    private long time;
+    private long start = System.currentTimeMillis();
+    private ArrayList<Sim> listSim;
+    private int day;
+    private static Timer instance;
 
-    public Timer() {
-        count = 0;
-        workerList = new ArrayList<>();
+    private Timer(ArrayList<Sim> listSim){
+        this.listSim = listSim;
+        time = 0;
+        day = 0;
+    }
+    private Timer(ArrayList<Sim> listSim,int day, long time){
+        this.time = time;
+        this.day = day;
+        this.listSim = listSim;
     }
 
-    public synchronized void start() {
-        Thread timerThread = new Thread(() -> {
-            while (true) {
-                int activeWorkers = 0;
-                for (Worker worker : workerList) {
-                    if (worker.isActive()) {
-                        activeWorkers++;
-                    }
-                }
-
-                if (activeWorkers > 0) {
-                    // At least one worker is active, so increment the count
-                    count++;
-                    System.out.println("Count is now: " + count);
-                } else {
-                    // No active workers, so suspend the thread and wait for a worker to be added
-                    try {
-                        System.out.println("No active workers, suspending timer thread...");
-                        synchronized(this) {
-                            wait();
-                        }
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                try {
-                    Thread.sleep(1000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        timerThread.start();
-    }
-
-    public synchronized void addWorker(Worker worker) {
-        workerList.add(worker);
-        worker.start();
-
-        // Notify the timer thread that a new worker has been added
-        System.out.println("Worker added, resuming timer thread...");
-        synchronized(this) {
-            notify();
+    public synchronized static void init(ArrayList<Sim> listSim){
+        if (instance == null){
+            instance = new Timer(listSim);
         }
+    }
+    
+    public synchronized static void Timer(ArrayList<Sim> listSim,int day, long time){
+        if (instance == null){
+            instance = new Timer(listSim, day, time);
+        }
+    }
+
+    public synchronized void setTime(){
+        start = System.currentTimeMillis();
+    }
+
+    public synchronized void updateTime(){
+        boolean allIdle = true;
+        for(Sim i : listSim){
+            if (!(i.getAction().isIdle())){
+                allIdle = false;
+            }
+        }
+        if (!allIdle){
+            time = time + System.currentTimeMillis() - start;
+            day = (time/720000);
+        }
+    }
+    public long getTime(){
+        return time;
+    }
+    public int getDay(){
+        return day;
     }
 }
--- src/entity/World.java
+++ src/entity/World.java
@@ -1,8 +1,10 @@
 package entity;
 
 import java.util.ArrayList;
-import entity.sim.Sim;
+
 import org.w3c.dom.ls.LSInput;
+
+import entity.sim.Sim;
 
 public class World {
     final int WORLD_LENGTH = 64;
@@ -14,51 +16,44 @@ public class World {
     private static World instance ;
 
     private World(){
-        Timer.init();
-        clock = Timer.getInstance();
+        
         listHouse = new ArrayList<House>();
         listSim = new ArrayList<Sim>();
+        Timer.init(listSim);
+        clock = Timer.getInstance();
     }
-
-    private World(Timer clock, ArrayList<House> listHouse, private ArrayList<Sim> listSim){
-        this.clock = clock;
+    private World(ArrayList<House> listHouse, ArrayList<Sim> listSim, int day, long time){
         this.listHouse = listHouse;
         this.listSim = listSim;
+        Timer.init( listSim ,day,time);
+        clock = Timer.getInstance();
     }
-
     public static World getInstance(){
         return instance;
     }
-
     public synchronized static void init(){
         if (instance == null){
             instance = new World();
         }
     }
-
-    public synchronized static void init(Timer clock, ArrayList<House> listHouse, private ArrayList<Sim> listSim){
+    public synchronized static void init(Timer clock, ArrayList<House> listHouse, ArrayList<Sim> listSim){
         if (instance == null){
             instance = new World(clock,listHouse,listSim);
         }
     }
-
     public synchronized void addHouse(House h){
         listHouse.add(h);
     }
-
     public synchronized void addSim(Sim s){
         listSim.add(s);
     }
-
     public ArrayList<House> getListHouse(){
         return listHouse;
     }
-
     public ArrayList<Sim> getListSim(){
         return listSim;
     }
-    
-    public int getTime(){
-        return clock.getTime();
+    public Timer getClock(){
+        return clock;
     }
 }
